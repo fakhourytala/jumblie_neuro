6f3a4745c4cff8b607b10a5925a36e8db528d5ea
Tala Fakhoury <87367226+fakhourytala@users.noreply.github.com>
2024-01-22 23:19:03 -0500

    Update dailyWords.js

--- dailyWords.js
+++ dailyWords.js
@@ -25,7 +25,7 @@ function getDayDifference() {
 // let puzzleNumber = (dayDifference % wordList.length) + 1;
 
 // number of the neuro - jumblie
-let dayDifference = 3;
-let puzzleId = 4;
+let dayDifference = 4;
+let puzzleId = 5;
 let wordsForTheDay = wordList[puzzleId - 1];
 let puzzleNumber = puzzleId;
