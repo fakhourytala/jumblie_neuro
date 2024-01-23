d34c1070d16dec3bb1c02da64c0f674feb9fb9cb
Tala Fakhoury <87367226+fakhourytala@users.noreply.github.com>
2024-01-22 22:10:47 -0500

    Update game.js

--- game.js
+++ game.js
@@ -18,7 +18,7 @@ let selectedButtons = [];
 let guessedWords = 0;
 let wrongGuesses = 0;
 
-let scoreString = `neuro Jumblie #${puzzleNumber}\n`;
+let scoreString = `Jumblie #${puzzleNumber}\n`;
 
 const letterMap = {};
 
